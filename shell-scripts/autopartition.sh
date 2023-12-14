ec5856b657b6c91f268737f9f17cdd5fb9a275a5
Semere Tsehaye Ghebregiorgish <semts703@gmail.com>
2023-12-14 16:00:37 +0100

    script to run partition

--- shell-scripts/autopartition.sh
+++ shell-scripts/autopartition.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-# The Original script comes from the repo with link https://gist.github.com/sozercan/e089f34fd148156facea27583c12e2e0
-
-
-# An set of disks to ignore from partitioning and formatting
-BLACKLIST="/dev/sda|/dev/sdb"
-# Base directory to hold the data* files
-DATA_BASE="/mnt"
+BLACKLIST="/dev/sda"
 
 usage() {
     echo "Usage: $(basename $0) <new disk>"
@@ -28,19 +21,6 @@ scan_for_new_disks() {
     echo "${RET}"
 }
 
-get_next_mountpoint() {
-    DIRS=($(ls -1d ${DATA_BASE}/data* 2>&1| sort --version-sort))
-    if [ -z "${DIRS[0]}" ];
-    then
-        echo "${DATA_BASE}/data1"
-        return
-    else
-        IDX=$(echo "${DIRS[${#DIRS[@]}-1]}"|tr -d "[a-zA-Z/]" )
-        IDX=$(( ${IDX} + 1 ))
-        echo "${DATA_BASE}/data${IDX}"
-    fi
-}
-
 add_to_fstab() {
     UUID=${1}
     MOUNTPOINT=${2}
@@ -49,44 +29,27 @@ add_to_fstab() {
     then
         echo "Not adding ${UUID} to fstab again (it's already there!)"
     else
-        LINE="UUID=\"${UUID}\"\t${MOUNTPOINT}\text4\tnoatime,nodiratime,nodev,noexec,nosuid\t1 2"
+        LINE="UUID=\"${UUID}\"\t${MOUNTPOINT}\txfs\tdefaults,nofail\t0 1"
+        echo "to be added ${LINE}"
         echo -e "${LINE}" >> /etc/fstab
     fi
 }
 
 is_partitioned() {
-# Checks if there is a valid partition table on the
-# specified disk
-    OUTPUT=$(sfdisk -l ${1} 2>&1)
-    grep "No partitions found" "${OUTPUT}" >/dev/null 2>&1
-    return "${?}"
-}
-
-has_filesystem() {
-    DEVICE=${1}
-    OUTPUT=$(file -L -s ${DEVICE})
-    grep filesystem <<< "${OUTPUT}" > /dev/null 2>&1
-    return ${?}
+   blkid ${1} 2>&1
+   return "${?}"
 }
 
 do_partition() {
 # This function creates one (1) primary partition on the
 # disk, using all available space
     DISK=${1}
-    echo "n
-p
-1
-w"| fdisk "${DISK}" > /dev/null 2>&1
-
-#
-# Use the bash-specific $PIPESTATUS to ensure we get the correct exit code
-# from fdisk and not from echo
-if [ ${PIPESTATUS[1]} -ne 0 ];
-then
-    echo "An error occurred partitioning ${DISK}" >&2
-    echo "I cannot continue" >&2
-    exit 2
-fi
+    parted "${DISK}" --script mklabel gpt mkpart xfspart xfs 0% 100%
+    mkfs.xfs "${DISK}1"
+    partprobe "${DISK}1"
+        mkdir /opt/sparkbeyond1
+    mount "${DISK}1" /opt/sparkbeyond1
+    lsblk -o NAME,HCTL,SIZE,MOUNTPOINT | grep -i "sd"
 }
 
 if [ -z "${1}" ];
@@ -105,20 +68,6 @@ do
         echo "${DISK} is not partitioned, partitioning"
         do_partition ${DISK}
     fi
-    PARTITION=$(fdisk -l ${DISK}|grep -A 1 Device|tail -n 1|awk '{print $1}')
-    has_filesystem ${PARTITION}
-    if [ ${?} -ne 0 ];
-    then
-        echo "Creating filesystem on ${PARTITION}."
-        #echo "Press Ctrl-C if you don't want to destroy all data on ${PARTITION}"
-        #sleep 5
-        mkfs -j -t ext4 ${PARTITION}
-    fi
-    MOUNTPOINT=$(get_next_mountpoint)
-    echo "Next mount point appears to be ${MOUNTPOINT}"
-    [ -d "${MOUNTPOINT}" ] || mkdir "${MOUNTPOINT}"
-    read UUID FS_TYPE < <(blkid -u filesystem ${PARTITION}|awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
-    add_to_fstab "${UUID}" "${MOUNTPOINT}"
-    echo "Mounting disk ${PARTITION} on ${MOUNTPOINT}"
-    mount "${MOUNTPOINT}"
+    UUID=$(blkid "${DISK}1" -o value -s UUID)
+    add_to_fstab "${UUID}" "/opt/sparkbeyond1"
 done
